77a75999fb214907611b1da678086ce63c53466a
Claudio Falabella <79667996+Klaud10@users.noreply.github.com>
2021-09-29 15:56:33 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { HooksApp } from './HooksApp';
+
+
+ReactDOM.render(
+    <MultipleCustomHooks />, 
+  document.getElementById('root')
+);
+
